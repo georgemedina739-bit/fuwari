e4ed34bb91577699cbab3c1f5c017efbfde90ba5
georgemedina739-bit <georgemedina739@gmail.com>
2026-05-08 19:39:32 +0800

    更新 config.ts

--- src/config.ts
+++ src/config.ts
@@ -57,18 +57,18 @@ export const profileConfig: ProfileConfig = {
 	name: "Lorem Ipsum",
 	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
 	links: [
-		{
-			name: "Twitter",
-			icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
+		//{
+			//name: "Twitter",
+			//icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
 			// You will need to install the corresponding icon set if it's not already included
 			// `pnpm add @iconify-json/<icon-set-name>`
-			url: "https://twitter.com",
-		},
-		{
-			name: "Steam",
-			icon: "fa6-brands:steam",
-			url: "https://store.steampowered.com",
-		},
+			//url: "https://twitter.com",
+		//},
+		//{
+			//name: "Steam",
+			//icon: "fa6-brands:steam",
+			//url: "https://store.steampowered.com",
+		//},
 		{
 			name: "GitHub",
 			icon: "fa6-brands:github",
