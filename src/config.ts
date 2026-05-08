8625180a2ec91a9ddb1345b64b1da8e6508fbeed
georgemedina739-bit <georgemedina739@gmail.com>
2026-05-08 19:48:18 +0800

    更新 config.ts

--- src/config.ts
+++ src/config.ts
@@ -55,7 +55,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Bad Pussy",
-	bio: "Abc",
+	bio: "Abcd",
 	links: [
 		//{
 			//name: "Twitter",
